44f4c47dd30ecc1b2e7d3e7ec37c2f3609f1b30a
Nurul Safitri <nrlsftr161@gmail.com>
2023-10-24 20:16:47 +0700

    perubahan widget

--- library_app/lib/pages/welcome_pages.dart
+++ library_app/lib/pages/welcome_pages.dart
@@ -2,8 +2,10 @@ part of 'pages.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
-
+  
+  
   @override
+  
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColor,
